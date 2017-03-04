504a08c6d01ded085fb1d7f7da4569b6d1f29ad8
Tal Peretz <13@1500.co.il>
2017-03-04 14:28:16 +0200

    facebook stuff

--- server.py
+++ server.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def is_alive():
     return jsonify({'success':True})
 
+@app.route('/', methods=['POST', 'GET'])
+def messenger():
+    content = request.get_json(silent=True)
+    return jsonify(content)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
